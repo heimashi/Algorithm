c62377fbf58ef24bbe866dee5664505b28e3630c
shiwang <shiwang@rong360.com>
2017-03-07 14:01:04 +0800

    init repo

---
+++ src/hello_c.c
@@ -0,0 +1,11 @@
+/*
+* compile: gcc hello_c.c
+*	   clang -o a hello_c.c
+* run: ./a.out
+*/
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+  printf("hello c\n");
+}
---
+++ src/hello_cpp.cpp
@@ -0,0 +1,12 @@
+/*
+*
+* g++ hello_cpp.cpp
+*
+*/
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  cout<<"hello"<<endl;
+}
